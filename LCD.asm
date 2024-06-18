42a1ebaf3be4760143dbb2a0a8de685d9931a28e
OG-NI <in.neu@online.de>
2024-06-18 07:27:34 +0200

    loaded dino into CGRAM

--- LCD.asm
+++ LCD.asm
@@ -1,53 +1,65 @@
-; Very basic demonstration example for HD44780 simulator
-; * Click on "Virtual HW" in the main menu,
-; * choose "Open",
-; * change filter to VH Component,
-; * open "LCD.vhc",
-; * press F2,
-; * press F6,
+; very basic demonstration example for hd44780 simulator
+; * click on "virtual hw" in the main menu,
+; * choose "open",
+; * change filter to vh component,
+; * open "lcd.vhc",
+; * press f2,
+; * press f6,
 ; * enjoy ... :-)
-; * press F2 to end.
+; * press f2 to end.
 ;
-; Note: simulated delays are skipped in this example.
+; note: simulated delays are skipped in this example.
 ;
-	org	0
-	jmp	start
+	ORG	0
+	JMP	init
 
-RS	bit	P3.0
-RW	bit	P3.1
-E	bit	P3.2
-D	equ	P1
+RS	BIT	P3.0
+RW	BIT	P3.1
+E	BIT	P3.2
+D	EQU	P1
 
-cmd	macro	cmd_code
-	setb	E
-	mov	D, cmd_code
-	clr	E
-endm
+CMD	MACRO	CMD_CODE
+	SETB	E
+	MOV	D, CMD_CODE
+	CLR	E
+ENDM
 
-string:	db	'Welcome to MCU 8051 IDE!\0'
+DINO:	DB	00h, 07h, 07h, 0ch, 1eh, 1ch, 14h, 12h, 0ffh
 
-start:	mov	D, #0
-	clr	RW
-	mov	DPTR, #string
+init:	MOV	D, #0
+	CLR	RW		; write mode for display
+	CLR	RS		; command mode
+	CMD	#01H		; clear display
+	;CMD	#02H		; cursor home
+	;CMD	#06H		; entry mode set
+	CMD	#0CH		; display on, cursor off
+	CMD	#00011110B	; cursor/display shift
+	;cmd	#00111100b	; function set
+	;cmd	#10000001b	; set ddram address
 
-main:	clr	RS
-	cmd	#00000001b	; Clear display
-	cmd	#00000010b	; Cursor home
-	cmd	#00000110b	; Entry mode set
-	cmd	#00001111b	; Display ON/OFF control
-	cmd	#00011110b	; Cursor/display shift
-	cmd	#00111100b	; Function set
-	cmd	#10000001b	; Set DDRAM address
-
-	; Print the string ...
-	setb	RS
-	mov	R0, #0
-print:	mov	A, R0
-	inc	R0
-	movc	A, @A+DPTR
-	cmd	A
-	cjne	A, #0, print
-	sjmp	main
-
+	; print the dino
+	cmd	#38h		; two lines and 5x7 matrix
+	cmd	#80h		; DDRAM address 0 (cursor to beginning of first line)
+	cmd	#40h		; CGRAM address 0
+	setb	rs		; data mode
+	mov	dptr, #dino
+	mov	r0, #0
+print:
+	mov	a, r0
+	inc	r0
+	movc	a, @a+dptr
+	cmd	a
+	cjne	a, #11111111b, print
+	cmd	#1h
 	end
+
+	; print the string ...
+;	SETB	RS
+;	MOV	R0, #0
+;PRINT:	MOV	A, R0
+;	INC	R0
+;	MOVC	A, @A+DPTR
+;	CMD	A
+;	CJNE	A, #0, PRINT
+;	SJMP	MAIN
 
