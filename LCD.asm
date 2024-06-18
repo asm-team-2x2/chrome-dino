9b7b1973f18773333ea9a5ba9fdec1bd8e15d97d
OG-NI <in.neu@online.de>
2024-06-18 07:27:34 +0200

    added cactus, bird characters, printed custom characters

--- LCD.asm
+++ LCD.asm
@@ -1,65 +1,48 @@
-; very basic demonstration example for hd44780 simulator
-; * click on "virtual hw" in the main menu,
-; * choose "open",
-; * change filter to vh component,
-; * open "lcd.vhc",
-; * press f2,
-; * press f6,
-; * enjoy ... :-)
-; * press f2 to end.
-;
-; note: simulated delays are skipped in this example.
-;
-	ORG	0
-	JMP	init
-
-RS	BIT	P3.0
-RW	BIT	P3.1
-E	BIT	P3.2
-D	EQU	P1
-
+; sends a command to the LCDs data pins
 CMD	MACRO	CMD_CODE
 	SETB	E
 	MOV	D, CMD_CODE
 	CLR	E
 ENDM
 
-DINO:	DB	00h, 07h, 07h, 0ch, 1eh, 1ch, 14h, 12h, 0ffh
+	ORG	0
+	JMP	START
 
-init:	MOV	D, #0
+RS	BIT	P3.0
+RW	BIT	P3.1
+E	BIT	P3.2
+D	EQU	P1
+
+CHARS:	DB	00H, 07H, 07H, 0CH, 1EH, 1CH, 14H, 12H, 04H, 04H, 05H, 17H, 1EH, 0EH, 06H, 06H, 0H, 10H, 1CH, 0FH, 0CH, 08H, 00H, 00H
+
+START:
+	MOV	D, #0		; clear data pins
 	CLR	RW		; write mode for display
-	CLR	RS		; command mode
+	CLR	RS		; select command register
 	CMD	#01H		; clear display
-	;CMD	#02H		; cursor home
-	;CMD	#06H		; entry mode set
+	CMD	#02H		; cursor home
 	CMD	#0CH		; display on, cursor off
-	CMD	#00011110B	; cursor/display shift
-	;cmd	#00111100b	; function set
-	;cmd	#10000001b	; set ddram address
+	CMD	#1EH		; cursor/display shift
 
-	; print the dino
-	cmd	#38h		; two lines and 5x7 matrix
-	cmd	#80h		; DDRAM address 0 (cursor to beginning of first line)
-	cmd	#40h		; CGRAM address 0
-	setb	rs		; data mode
-	mov	dptr, #dino
-	mov	r0, #0
-print:
-	mov	a, r0
-	inc	r0
-	movc	a, @a+dptr
-	cmd	a
-	cjne	a, #11111111b, print
-	cmd	#1h
-	end
+; load custom characters
+	CMD	#40H		; set CGRAM address
+	SETB	RS		; select data register
+	MOV	DPTR, #CHARS
+	MOV	R0, #0
+LOOP_0:
+	MOV	A, R0
+	INC	R0
+	MOVC	A, @A+DPTR
+	CMD	A
+	CJNE	R0, #24, LOOP_0
 
-	; print the string ...
-;	SETB	RS
-;	MOV	R0, #0
-;PRINT:	MOV	A, R0
-;	INC	R0
-;	MOVC	A, @A+DPTR
-;	CMD	A
-;	CJNE	A, #0, PRINT
-;	SJMP	MAIN
+; test print characters
+	CLR	RS		; select command register
+	CMD	#81H		; set DDRAM address (cursor to beginning of first line)
+	SETB	RS		; select data register
+	CMD	#0
+	CMD	#1
+	CMD	#2
+	SJMP	END
 
+END:	SJMP	END
